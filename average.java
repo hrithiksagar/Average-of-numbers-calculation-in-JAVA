993f5d6e70213d5c4aee3e48d374df6589841921
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2019-10-07 14:26:46 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,16 @@
+class average
+{
+public static void main(String args[])
+{
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+int c = Integer.parseInt(args[2]);
+int d = Integer.parseInt(args[3]);
+int e = Integer.parseInt(args[4]);
+int f =a+b+c+d+e;
+int g=f/5;
+System.out.println(g);
+
+}
+}
+
